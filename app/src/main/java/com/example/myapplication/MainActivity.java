06bb5339d4ee16283c6ae1e02d855a104a6ec90e
shaozhiyang <shaozhiyang@jinbirds.com>
2019-08-02 21:39:37 +0800

    第四次提交

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // 今天事个好天气
+        // master
     }
 }
